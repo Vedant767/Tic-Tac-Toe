b529b6054056b882dea1a9c1c1420abd533bc171
Vedant <vedantgire48@gmail.com>
2024-06-01 18:14:52 +0530

    Layout and basic functionality working

--- src/Components/TicTacToe.js
+++ src/Components/TicTacToe.js
@@ -1,5 +1,42 @@
+import { useState } from "react";
+import { CONSTANTS } from "../constants";
+
 const TicTacToe = () => {
-  return <h1>Inside Tic Tac Toe</h1>;
+  const noOfBlocks = new Array(9).fill(null);
+  const [board, setBoard] = useState(noOfBlocks);
+  const [player, setPlayer] = useState("O");
+
+  const boardHandler = (index) => {
+    board[index] = player;
+
+    setBoard(board);
+    const playerValue = player === "O" ? "X" : "O";
+    setPlayer(playerValue);
+  };
+
+  const resetAction = () => setBoard(noOfBlocks);
+
+  return (
+    <div className="tic-tac-toe">
+      <div className="tic-tac-toe-inner-text">
+        <h3>{CONSTANTS.PLAYER_TEXT.replace("player_name", player)}</h3>
+        <button onClick={resetAction}>{CONSTANTS.BUTTON_TEXT}</button>
+      </div>
+      <div className="board">
+        {board.map((value, index) => {
+          return (
+            <button
+              className="board-block"
+              onClick={() => boardHandler(index)}
+              disabled={value !== null}
+            >
+              {value}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TicTacToe;
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const CONSTANTS = {
+  PLAYER_TEXT: "Player player_name turn",
+  BUTTON_TEXT: "Reset Option",
+};
